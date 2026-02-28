95d64d3fa73cfd7053f5a3a843050bf8a1f2bb04
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-02-28 15:49:51 +0100

    add socket.io for realtime communications

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import http from 'http';
 import { toNodeHandler } from 'better-auth/node';
 import { auth } from './lib/auth';
 import { usersRouter } from './users/users.router';
 import { chatsRouter } from './chats/chats.router';
 import { messagesRouter } from './messages/messages.router';
+import { Server, Socket } from 'socket.io';
 
 const PORT = process.env.PORT || 8000;
 const app = express();
+const server = http.createServer(app);
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000'],
     credentials: true,
   }),
 );
@@ -24,6 +27,21 @@ app.use('/api/users', usersRouter);
 app.use('/api/chats', chatsRouter);
 app.use('/api/messages', messagesRouter);
 
-app.listen(PORT, () => {
+const io = new Server(server, {
+  cors: {
+    origin: ['http://localhost:3000'],
+  },
+});
+
+io.on('connection', (socket: Socket) => {
+  console.log('Socket connected');
+  socket.on('message', async (message) => {
+    console.log(message);
+    io.emit('message', message);
+  });
+  socket.on('disconnect', () => console.log('User disconnected'));
+});
+
+server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
